00497458f05ca51668ec77714f6e987ba15397b7
abhishek chauhan <47456475+abhishekchauhan00@users.noreply.github.com>
2022-04-16 11:19:40 +0530

    initial commit

---
+++ dbconnection.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$servername= "localhost";
+$dbuser= "root";
+$password = "@#$%1234";
+
+$dbname = "amps";
+
+$conn = mysqli_connect("$servername", "$dbuser", "$password", "$dbname");
+
+if (!$conn) {
+	echo "Connection failed!";
+}
+?>
